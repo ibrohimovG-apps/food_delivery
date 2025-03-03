dc09c03a290f4a4f79d6dd5892eef91cfa91ed8e
macbook <gulomnaziribrohimov@gmail.com>
2025-03-03 14:44:26 +0500

    google maps api key for both ios and android

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,7 @@ import UIKit
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    GMSServices.provideAPIKey("AIzaSyAYSl8WkGGmKfYakQ1ZNm1J6zr72kJFvX8")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
